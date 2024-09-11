1eda16228ed82adba374f809b6db76889a0015ee
Jas_Radadiya <jasminradadiya29@gmail.com>
2024-09-11 19:20:41 +0530

    added watch history controller

--- src/db/index.js
+++ src/db/index.js
@@ -9,8 +9,7 @@ const connectDB = async () => {
     console.log(
       `\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`
     );
-  } 
-  catch (error) {
+  } catch (error) {
     console.log("MONGODB Connection FAILED !!", error);
     process.exit(1);
   }
